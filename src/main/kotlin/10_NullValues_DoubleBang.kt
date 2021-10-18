2a333cc31eed8afe7afa1426ac0ef84d8667e245
cesardramirez <cesardavid89@gmail.com>
2021-10-18 18:14:23 -0500

    feat(nullSafety/doubleBang).

---
+++ src/main/kotlin/10_NullValues_DoubleBang.kt
@@ -0,0 +1,25 @@
+package main.kotlin
+
+fun main() {
+    val dato : Int? = null  // null como valor por defecto.
+    val msg: String? = null  // El ? es necesario para inicializar un valor en nulo.
+
+    // Saldrá error en el IDE si no se coloca el ? ó !!, lo cuál el IDE mostrará las posibles opciones para validar el dato.
+
+    val numero : Int? = null
+    // Antes.
+    if (numero != null) {  // No ingresa y no realiza la operación.
+        println(numero.div(2))
+    }
+    // Ahora
+    println(numero?.div(2))  // No realiza la operación y por defecto imprime el valor null.
+
+    val listWithNulls: List<String?> = listOf("Kotlin", null, "Yeih")
+    for (item in listWithNulls) {
+        item?.let { print("$it \t") }  // Imprime sólo los valores no nulos.
+    }
+
+    println()
+    println(dato?.inc())  // Si el valor es null, cualquier método adicional que se llame (por defecto) su resultado será null.
+    println(msg!!.length)  // Si el valor es null, forzará a que se lance la excepción, en este caso, un NPE.
+}
